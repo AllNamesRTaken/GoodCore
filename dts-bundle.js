1c3905cfc74003ee449b1a62442b4d8ffe723f27
AllNamedRTaken <joel.brage@gmail.com>
2017-04-15 11:51:37 +0200

    ..

--- dts-bundle.js
+++ dts-bundle.js
@@ -2,6 +2,6 @@ var dts = require('dts-bundle');
 var conf = require("./dts-bundle.json");
 var packageJson = require("./package.json");
 conf.name = packageJson.name;
-conf.out = "../../"+packageJson.name+".d.ts";
+conf.out = packageJson.name+".d.ts";
 
 dts.bundle(conf);
