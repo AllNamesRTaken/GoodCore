bb4ca4bdd12a95550bc5635be643cca280239222
AllNamedRTaken <joel.brage@gmail.com>
2017-04-15 13:00:19 +0200

    .

--- minify-umd.js
+++ minify-umd.js
@@ -2,8 +2,8 @@ var fs = require("fs");
 var UglifyJS = require("uglify-js");
 var packageJson = require("./package.json");
 
-var distInFile = "./dist/lib/"+packageJson.name+".umd.js";
-var distOutFileUnversioned = "./dist/lib/"+packageJson.name+".umd.min.js";
+var distInFile = "./dist/"+packageJson.name+".umd.js";
+var distOutFileUnversioned = "./dist/"+packageJson.name+".umd.min.js";
 
 var result = UglifyJS.minify(distInFile, { mangle: true });
 fs.writeFileSync(distOutFileUnversioned, result.code, { encoding: "utf-8" });
