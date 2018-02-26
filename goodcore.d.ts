4fb2a6d31173f6e95fac5a8d95c959b2e2926d9e
Joel Brage <joel.brage@zafari.se>
2018-02-26 09:23:33 +0100

    fixed .d.ts

--- goodcore.d.ts
+++ goodcore.d.ts
@@ -469,18 +469,28 @@ declare module 'goodcore/struct/IndexedTree' {
     import { Dictionary } from "goodcore/struct/Dictionary";
     import { Tree } from "goodcore/struct/Tree";
     export class IndexedTree<T> extends Tree<T> {
-		index: Dictionary<Tree<T>>;
-        indexer: (node: Tree<T>) => string | number;
-        count: number;
-        static fromObject<T>(obj: any, indexer?: (node: Tree<T>) => string | number): Tree<T>;
-        constructor(id?: string | number, indexer?: (node: Tree<T>) => string | number, index?: Dictionary<Tree<T>>);
-        protected create<S = T>(...args: any[]): Tree<S>;
-        insertAt(pos: number, data: T, id?: string | number, updateIndex?: boolean): void;
-		addTo(parentId: string | number, data: T|Tree<T>, id?: string | number, updateIndex?: boolean): IndexedTree<T> | undefined;
-        add(data: T | Tree<T>, id?: string | number, updateIndex?: boolean): void;
-        contains(node: Tree<T> | string | number): boolean
-        get(id: string | number): IndexedTree<T> | undefined;
-        cut(): IndexedTree<T>;
+		init(obj: Partial<IndexedTree<T>>): IndexedTree<T>;
+		index: IndexedTree<Tree<T>>;
+		indexer: (node: IndexedTree<T>) => string | number;
+		count: number;
+		static fromObject<T>(obj: any, indexer?: (node: IndexedTree<T>) => string | number): Tree<T>;
+		static fromNodeList<S, T>(nodes: S[], mapcfg?: {
+			id?: ((node: S) => string | number)|string | number, 
+			parent?: ((node: S) => string | number)|string | number, 
+			data?: ((node: S) => any)|string
+		}, virtualRoot?: boolean): Tree<T>;
+		constructor(id?: string | number, indexer?: (node: IndexedTree<T>) => string | number, index?: Dictionary<Tree<T>>);
+		protected create<S = T>(...args: any[]): Tree<S>;
+		insertAt(pos: number, data: T, id?: string | number, updateIndex?: boolean): void;
+		addTo(parentId: string | number, data: T|IndexedTree<T>, id?: string | number, updateIndex?: boolean): IndexedTree<T> | undefined;
+		add(data: T | IndexedTree<T>, id?: string | number, updateIndex?: boolean): void;
+		contains(node: IndexedTree<T> | string | number): boolean
+		get(id: string | number): IndexedTree<T> | undefined;
+		cut(): IndexedTree<T>;
+		reIndex(): void;
+		clone(): IndexedTree<T>;
+		prune(): IndexedTree<T>;
+		filter(condition: (node: Tree<T>) => boolean, parent?: Tree<T> | null): IndexedTree<T>;
     }
 }
 
