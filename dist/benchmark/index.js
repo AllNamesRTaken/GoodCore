673eacc6ed95fc718be5a7735b6ac4007694c071
Joel Brage <joel.brage@zafari.se>
2018-10-17 20:29:41 +0200

    reverted back to es5

--- dist/benchmark/index.js
+++ dist/benchmark/index.js
@@ -1,3 +1,3 @@
 import { suites as ArrBench } from "./ArrBench";
-ArrBench.forEach((el) => el.run({ 'async': false }));
+ArrBench.forEach(function (el) { return el.run({ 'async': false }); });
 //# sourceMappingURL=index.js.map
